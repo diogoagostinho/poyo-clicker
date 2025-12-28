6038603ceceb93e4ad9d9247545512b8f5ccfd00
AGOSTINHO <LaaGostim.OFICIAL@gmail.com>
2025-12-28 23:02:42 +0000

    v0.1.3a
    Scroll on customize works on skins only, need to fix for backgrounds and music

--- Assets/Scripts/CustomizeTabs.cs
+++ Assets/Scripts/CustomizeTabs.cs
@@ -6,6 +6,10 @@ public class CustomizeTabs : MonoBehaviour
     public GameObject backgroundsContent;
     public GameObject musicContent;
 
+    public GameObject skinsScrollView;
+    public GameObject backgroundsScrollView;
+    public GameObject musicScrollView;
+
     void Start()
     {
         ShowSkins();
@@ -16,6 +20,10 @@ public class CustomizeTabs : MonoBehaviour
         skinsContent.SetActive(true);
         backgroundsContent.SetActive(false);
         musicContent.SetActive(false);
+
+        skinsScrollView.SetActive(true);
+        backgroundsScrollView.SetActive(false);
+        musicScrollView.SetActive(false);
     }
 
     public void ShowBackgrounds()
@@ -23,6 +31,10 @@ public class CustomizeTabs : MonoBehaviour
         skinsContent.SetActive(false);
         backgroundsContent.SetActive(true);
         musicContent.SetActive(false);
+
+        skinsScrollView.SetActive(false);
+        backgroundsScrollView.SetActive(true);
+        musicScrollView.SetActive(false);
     }
 
     public void ShowMusic()
@@ -30,5 +42,9 @@ public class CustomizeTabs : MonoBehaviour
         skinsContent.SetActive(false);
         backgroundsContent.SetActive(false);
         musicContent.SetActive(true);
+
+        skinsScrollView.SetActive(false);
+        backgroundsScrollView.SetActive(false);
+        musicScrollView.SetActive(true);
     }
 }
